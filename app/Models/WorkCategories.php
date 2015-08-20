89c4126cc52e79a31e0d57f44c51e5ce4c9c3a34
Soranad Chaipet <oldman.beta@gmail.com>
2015-08-20 12:08:29 +0700

    edit model work CMS

---
+++ app/Models/WorkCategories.php
@@ -0,0 +1,18 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImageTag extends Model {
+
+	protected $table = 'work_categories';
+    
+    public static $save_rules = array(
+        "name" => "required|unique:categories"
+    );
+	
+	public function posts()
+	{
+	    return $this->belongsToMany('App\Models\WorkPost', 'work_post_categories', 'categories_id', 'work_id')->withTimestamps();;
+	}
+
+}
