f5b503d76accd927d639697498aba38d517cc3cd
intouch <intouchxiii@gmail.com>
2015-09-03 15:43:19 +0700

    work-sub menu

--- resources/views/web-layouts/works-header-detail.blade.php
+++ resources/views/web-layouts/works-header-detail.blade.php
@@ -1,29 +1,29 @@
 <header>
-    <a id="header-brand" data-fn="open">
+    <a id="header-brand" data-fn="close">
         <span id="header-brand-logo"></span>
         <h6 id="header-page-text">All Work &nbsp<i class="fa fa-chevron-down"></i></h6>
     </a>
-    <div>
+    <div class="work-menu-container">
 
         <ul class="work-sub-menu">
             <li class="circuit-v-row ">
                 <h6>
-                    <a href="http://localhost/kiwilauncher/public" class="menu-item-link">Home</a>
+                    <a href="http://localhost/kiwilauncher/public" class="menu-item-link">Web Application</a>
                 </h6>
             </li>
             <li class="circuit-v-row ">
                 <h6>
-                    <a href="http://localhost/kiwilauncher/public" class="menu-item-link">Home</a>
+                    <a href="http://localhost/kiwilauncher/public" class="menu-item-link">Mobile Application</a>
                 </h6>
             </li>
             <li class="circuit-v-row ">
                 <h6>
-                    <a href="http://localhost/kiwilauncher/public" class="menu-item-link">Home</a>
+                    <a href="http://localhost/kiwilauncher/public" class="menu-item-link">Graphic Design</a>
                 </h6>
             </li>
             <li class="circuit-v-row ">
                 <h6>
-                    <a href="http://localhost/kiwilauncher/public" class="menu-item-link">Home</a>
+                    <a href="http://localhost/kiwilauncher/public" class="menu-item-link">Banner</a>
                 </h6>
             </li>
         </ul>
