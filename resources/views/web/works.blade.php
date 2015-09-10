b55c76e68ec1b9a0ce332cdff049176ed25e6b2a
Soranad Chaipet <oldman.beta@gmail.com>
2015-09-10 17:07:23 +0700

    edit link in all work.

--- resources/views/web/works.blade.php
+++ resources/views/web/works.blade.php
@@ -21,67 +21,17 @@
 
 <div id="works">
     <section id="works-content" class="section-frame">
-        <ul id="works-list" class="small-block-grid-1 medium-block-grid-2 multi-col-wrapper">
-            <li class="has-mg-b">
-                <a class="figure-link work-figure" href="{!! URL::route('worksDrseoul') !!}">
-                    <!--<img class="figure-img work-cvimg" src="http://placehold.it/800x450" />-->
-                    {!! HTML::image("image/works/drseoul-brand-logo.png", "Some app", array(
-                        "class" => "figure-img work-cvimg")
-                    ) !!}
-                    <div class="figure-layer"></div>
-                </a>
-                <div class="work-text has-pd-lr">
-                    <h4><a href="{!! URL::route('worksDrseoul') !!}">Dr.seoul</a></h4>
-                    <p>Web Application</p>
-                    <p>Branding, e-commerce</p>
-                </div>
-            </li>
-            <li class="has-mg-b">
-                <a class="figure-link work-figure">
-                    {!! HTML::image("image/sample/sandawe-1.jpg", "Some web", array(
-                        "class" => "figure-img work-cvimg")
-                    ) !!}
-                    <div class="figure-layer"></div>
-                </a>
-                <div class="work-text has-pd-lr">
-                    <h4><a>Some Website</a></h4>
-                    <p>Website</p>
-                </div>
-            </li>
-            <li class="has-mg-b">
-                <a class="figure-link work-figure">
-                    {!! HTML::image("image/sample/fiat-thumb.jpg", "Some app", array(
-                        "class" => "figure-img work-cvimg")
-                    ) !!}
-                    <div class="figure-layer"></div>
-                </a>
-                <div class="work-text has-pd-lr">
-                    <h4><a>Another</a></h4>
-                    <p>Website / Branding</p>
-                </div>
-            </li>
-            <li class="has-mg-b">
-                <a class="figure-link work-figure">
-                    {!! HTML::image("image/sample/pairi-daiza-thumb.jpg", "Some web", array(
-                        "class" => "figure-img work-cvimg")
-                    ) !!}
-                    <div class="figure-layer"></div>
-                </a>
-                <div class="work-text has-pd-lr">
-                    <h4><a>1234567</a></h4>
-                    <p>Website / Graphic Design</p>
-                </div>
-            </li>
+        <ul id="works-list" class="small-block-grid-1 medium-block-grid-2 multi-col-wrapper">          
             @foreach($works as $work)
                 <li class="has-mg-b">
-                <a class="figure-link work-figure" href="{!! "works/".$work->url !!}">
+                <a class="figure-link work-figure" href="{!! URL::route('work-post', array($work->url)) !!}">
                     {!! HTML::image("$work->feature_image_url", "Some web", array(
                         "class" => "figure-img work-cvimg")
                     ) !!}
                     <div class="figure-layer"></div>
                 </a>
                 <div class="work-text has-pd-lr">
-                    <h4><a href="{!! "works/".$work->url !!}">{!! $work->title !!}</a></h4>
+                    <h4><a href="{!! URL::route('work-post', array($work->url)) !!}">{!! $work->title !!}</a></h4>
                     @foreach($work->categories as $category)
                         <p> {!! $category->name !!} </p>
                     @endforeach
